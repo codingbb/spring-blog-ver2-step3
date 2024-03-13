620895c460ff771415014d97780c6d20cc2dd77e
SCHcoding <codingb007@gmail.com>
2024-03-13 17:32:57 +0900

    ORM 게시글 상세보기

---
+++ src/test/java/shop/mtcoding/blog/BoardRepositoryTest.java
@@ -0,0 +1,29 @@
+package shop.mtcoding.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import shop.mtcoding.blog.board.Board;
+import shop.mtcoding.blog.board.BoardRepository;
+
+@Import(BoardRepository.class)
+@DataJpaTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void findById_test() {
+        int id = 1;
+        System.out.println("start - 1");
+        Board board = boardRepository.findById(id);
+        System.out.println("start - 2");
+        System.out.println(board.getUser().getId());
+        System.out.println("start - 3");
+        System.out.println(board.getUser().getUsername());
+
+    }
+
+}
